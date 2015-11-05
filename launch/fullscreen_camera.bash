8e7f0867b8b9fe43d499914571064d0ed2cc0960
Carlos Costa <carloscosta.cmcc@gmail.com>
2015-11-05 14:26:41 +0000

    Preliminary welding tests.

--- launch/fullscreen_camera.bash
+++ launch/fullscreen_camera.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 process_name=${1:-'image_view'}
-window_name=${2:-'/virtual_camera/image_rect'}
+window_name=${2:-'/virtual_camera/image_raw'}
 
 until ps -e | grep ${process_name} ; do sleep 1; done
 until wmctrl -r ${window_name} -b toggle,fullscreen ; do sleep 1; done
