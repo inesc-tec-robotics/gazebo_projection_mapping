a6eef48c8eb7009694e0b3f846bbc07f5af2690b
Carlos Costa <carloscosta.cmcc@gmail.com>
2016-04-21 14:38:55 +0200

    Added welding objects to world model.

--- launch/calibration_mouse_clicks.bash
+++ launch/calibration_mouse_clicks.bash
@@ -6,12 +6,17 @@ exposure_time_sec_pos=${3:-'0.25'}
 
 echo "Sending mouse events for ${number_patterns} patterns"
 
-xdotool mousemove 440 815 click 3
+#xdotool mousemove 440 815 click 3
+rosservice call /image_file_saver_raw/save
+rosservice call /image_file_saver_rect/save
+
 for (( i=1; i<=${number_patterns}; i++ ))
 do
 	echo "Send mouse events number ${i}"
 	sleep ${exposure_time_sec_pre}
-	xdotool mousemove 440 815 click 3
+	#xdotool mousemove 440 815 click 3
+	rosservice call /image_file_saver_raw/save
+	rosservice call /image_file_saver_rect/save
 	sleep ${exposure_time_sec_pos}
 	xdotool mousemove 440 415 click 1
 done
