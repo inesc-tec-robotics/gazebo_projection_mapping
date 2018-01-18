573fc344833b2a2e72bb25e3bd4c4887f3d44e62
Carlos Costa <carloscosta.cmcc@gmail.com>
2018-01-18 15:56:44 +0000

    Improved fullscreen script for making window on top of ubuntu bar.

--- launch/fullscreen_projection.bash
+++ launch/fullscreen_projection.bash
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-process_name=${1:-'scan3d-capture'}
-window_name=${2:-'scan3d-capture'}
+process_name=${1:-'image_view'}
+window_name=${2:-'/virtual_camera/image_raw'}
 
 until ps -e | grep ${process_name} ; do sleep 1; done
-until wmctrl -r ${window_name} -b toggle,fullscreen ; do sleep 1; done
-
+sleep 1
+until wmctrl -r ${window_name} -b toggle,fullscreen,above ; do sleep 1; done
